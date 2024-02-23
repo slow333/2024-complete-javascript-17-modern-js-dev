513ddb5ddc53453105e8824cabfe3b96cd1a75ba
kalpa <kalpa@kakao.com>
2024-02-23 15:50:33 +0900

    so so

--- clean.js
+++ clean.js
@@ -1,4 +1,4 @@
-var budget = [
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,58 +7,40 @@ var budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-var limits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
-};
-
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
+});
+spendingLimits.jay = 200;
+const getLimit = (user) => spendingLimits?.[user] ?? 0;
+const addExpense = function (value, description, user = 'Jonas') {
   user = user.toLowerCase();
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
-
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
+  if(value <= getLimit(user))
+     budget.push({ value: -value, description, user });
 };
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
-console.log(budget);
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
 
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
-  }
+
+const check = function () {
+  budget.forEach(bg => bg.value < -getLimit(bg.user) ? bg.flag = 'limit': '')
+  // for (const entry of budget)
+  //   if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
 };
 check();
 
-console.log(budget);
-
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
-  }
+const bigExpenses = function (bigLimit) {
+  let output = budget.map(bg =>
+    (bg.value <= -bigLimit) ? bg.description.slice(-2) + ' / ': '').join('')
+  // for (const entry of budget)
+  //     output += (entry.value <= -bigLimit) ? entry.description.slice(-2) + ' / ': ''; // Emojis are 2 chars
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
+console.log(budget);
+bigExpenses(1000)
